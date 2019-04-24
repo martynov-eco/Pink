5a2ea5911b41aadd40652f5cf65effe8d478f023
martynov-eco <martynov.post@gmail.com>
2019-04-24 20:12:39 +0400

    Ошибки после test, исправил

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,44 +1,3 @@
-/*var feedBack = document.querySelector(".button-write-us");
-var feedBackModal = document.querySelector(".modal-write-us");
-var closeFeedBack = feedBackModal.querySelector(".modal-close");
-var userName = feedBackModal.querySelector("[name=users-name]");
-var userEmail = feedBackModal.querySelector("[name=users-email]");
-var userMessage = feedBackModal.querySelector("[name=users-message]");
-var form = feedBackModal.querySelector("form");
-
-feedBack.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		feedBackModal.classList.add("modal-show");
-		userName.focus();
-});
-
-closeFeedBack.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		feedBackModal.classList.remove("modal-show");
-		feedBackModal.classList.remove("modal-error");
-});
-
-window.addEventListener("keydown", function (evt) {
-		if (evt.keyCode === 27) {
-				evt.preventDefault();
-				if (feedBackModal.classList.contains("modal-show")) {
-						feedBackModal.classList.remove("modal-show");
-						feedBackModal.classList.remove("modal-error");
-				}
-		}
-});
-
-form.addEventListener("submit", function (evt) {
-		if (!userName.value || !userEmail.value || !userMessage) {
-				evt.preventDefault();
-				feedBackModal.classList.remove("modal-error");
-				feedBackModal.offsetWidth = feedBackModal.offsetWidth;
-				feedBackModal.classList.add("modal-error");
-		}
-});*/
-
-
-
 var submitButton = document.querySelector(".form__button");
 var submitModal = document.querySelector(".modal-accept");
 var requiredModal = document.querySelector(".modal-required")
@@ -48,7 +7,6 @@ var form = document.querySelector(".form__wrapper");
 var userSurame = document.querySelector("[name=surname]");
 var userName = document.querySelector("[name=name]");
 var userEmail = document.querySelector("[name=email]");
-
 
 form.addEventListener("submit", function (evt) {
   if (!userName.value || !userSurname.value || !userEmail.value) {
@@ -69,4 +27,3 @@ closeRequiredModal.addEventListener("click", function (evt) {
   evt.preventDefault();
   requiredModal.classList.remove("modal-required--open");
 });
-
