e9414fd24a8c5fd5bc6f4f303229c80a2d942f70
martynov-eco <martynov.post@gmail.com>
2019-05-06 14:41:14 +0400

    внес правки после предзащиты

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ var navMain = document.querySelector('.page-header__top');
 var navToggle = document.querySelector('.page-header__button');
 
 navMain.classList.remove('page-header__top--nojs');
+navMain.classList.remove('page-header__top--opened');
+navMain.classList.add('page-header__top--closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('page-header__top--closed')) {
