37b975527dad0379843e7c662c1c4aa7a5cc7d72
martynov-eco <martynov.post@gmail.com>
2019-08-15 13:01:04 +0400

    Оживил слайдеры с помощью JS

--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,43 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('page-header__top--opened');
   }
 });
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 2;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+  showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+  showSlides(slideIndex -= 1);
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+/* Основная функция сладера */
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider__item");
+  var dots = document.getElementsByClassName("slider__dots-item");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slider__dots-item--active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " slider__dots-item--active";
+}
